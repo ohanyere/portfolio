52d870faf509b05538818aa6fbd52d6737ff03e0
Ohanyere Francis <mooref068@gmail.com>
2025-10-08 14:29:59 -0700

    style(hero.tsx): fix hero style issues

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { HandIcon, Code2Icon } from "lucide-react";
 
 export default function Hero() {
   return (
@@ -13,23 +14,35 @@ export default function Hero() {
         <motion.p
           whileHover={{ scale: 1.05 }}
           transition={{ type: "spring", stiffness: 200, damping: 12 }}
-          className="text-2xl sm:text-3xl font-semibold tracking-wide text-neutral-700"
+          className="flex justify-center items-center gap-2 text-2xl sm:text-4xl font-semibold tracking-wide text-neutral-700"
         >
           Hi, I’m
+          <motion.span
+            animate={{ rotate: [0, 20, 0] }}
+            transition={{ repeat: Infinity, repeatDelay: 2, duration: 1 }}
+          >
+            <HandIcon className="w-12 h-12 text-orange-400" />
+          </motion.span>
         </motion.p>
 
         {/* Hero Name */}
         <motion.h1
           whileHover={{ scale: 1.08 }}
           transition={{ type: "spring", stiffness: 300, damping: 10 }}
-          className="font-heading font-extrabold text-7xl sm:text-8xl md:text-9xl tracking-wide leading-tight cursor-move"
+          className="flex justify-center items-center gap-3 font-heading font-extrabold text-black text-7xl sm:text-8xl md:text-9xl tracking-wide leading-tight cursor-move"
         >
           Francis<span className="ml-2">.</span>
+          <motion.span
+            whileHover={{ rotate: 15 }}
+            transition={{ type: "spring", stiffness: 200, damping: 10 }}
+          >
+            <Code2Icon className="w-10 h-10 text-orange-400" />
+          </motion.span>
         </motion.h1>
 
         {/* Description */}
         <p className="text-base sm:text-lg leading-relaxed tracking-wide max-w-2xl mx-auto">
-          I’m Francis —<span className="text-orange-300"> a test-driven front-end developer who builds well-tested, high-performance applications using React, TypeScript, Cypress for E2E testing, and React Testing Library for component testing.</span> I focus on clean architecture, clarity, and reliability.
+          I’m Francis —<span className=""> a test-driven front-end developer who builds well-tested, high-performance applications using React, TypeScript, Cypress for E2E testing, and React Testing Library for component testing.</span> I focus on clean architecture, clarity, and reliability.
         </p>
 
         {/* Links */}
