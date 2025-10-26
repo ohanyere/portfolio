9f715e32bd2fdcad01a51694545d7eb9470b3bb9
Ohanyere Francis <mooref068@gmail.com>
2025-10-26 06:47:50 -0700

    fix(Project.ts): fix repo link

--- src/data/PROJECTS.ts
+++ src/data/PROJECTS.ts
@@ -6,10 +6,10 @@ const PROJECTS: Project[] = [
   title: "AnimalCheck ai",
   subtitle: "Livestock Diagnosis & Instant Disease Detection",
   description:
-    "📸 Snap a pic of sick livestock and get instant AI-powered disease detection with personalized care tips. Built with React, TypeScript, React Query, Node.js, Express, and Gemini AI, with full test coverage using Cypress and React Testing Library.",
+    "Snap a pic of sick livestock and get instant AI-powered disease detection with personalized care tips. Built with React, TypeScript, React Query, Node.js, Express, and Gemini AI, with full test coverage using Cypress and React Testing Library.",
   image: "/preview2.png",
   demo: "https://animalcheck-c5e5fbdbad00.herokuapp.com/",
-  repo: "#",
+  repo: "https://github.com/ohanyere/animal-check",
   tech: [
     "React",
     "TypeScript",
